ed450a055b157a164b42c537d5924bfb857b6a5e
0xmikki <90579247+MrBloomguy@users.noreply.github.com>
2024-01-26 15:43:47 +0000

    radio

--- qfpanelsource/src/views/claimed-points.tsx
+++ qfpanelsource/src/views/claimed-points.tsx
@@ -161,47 +161,13 @@ const TableData = () => (
                 </span>
               </td>
               <td className="whitespace-nowrap px-3 py-4 flex items-center text-sm gap-2 text-white/80">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={26}
-                  height={33}
-                  fill="none"
-                >
-                  <g clipPath="url(#a)">
-                    <rect
-                      width={26}
-                      height={32}
-                      y={0.329}
-                      fill="#04020F"
-                      fillOpacity={0.91}
-                      rx={3}
-                    />
-                    <path
-                      fill="#02E2AC"
-                      fillRule="evenodd"
-                      d="M20.976 12.895a8.516 8.516 0 0 1 1.583 4.99 8.5 8.5 0 0 1-1.583 4.917l2.16 1.644a11.196 11.196 0 0 0 2.13-6.546 11.22 11.22 0 0 0-2.293-6.856l-1.239 1.117-.758.734Z"
-                      clipRule="evenodd"
-                    />
-                    <path
-                      fill="#F2F8FD"
-                      fillRule="evenodd"
-                      d="m25.058 25.902-7.262-5.523c.518-.682.814-1.51.858-2.37a4.26 4.26 0 0 0-.65-2.428l3.475-3.14 3.638-3.405a.59.59 0 0 0 .015-.844l-.503-.504a14.353 14.353 0 0 0-9.746-4.265l.015-1.999c.014-.133 0-.266-.03-.4a1.034 1.034 0 0 0-.192-.355.881.881 0 0 0-.326-.237.862.862 0 0 0-.384-.089.862.862 0 0 0-.385.09c-.118.059-.236.133-.325.236a.877.877 0 0 0-.192.34.74.74 0 0 0-.03.4l-.015 2.059a14.03 14.03 0 0 0-2.588.503l.015-2.561a1.22 1.22 0 0 0-.03-.4 1.036 1.036 0 0 0-.192-.356.882.882 0 0 0-.325-.237.862.862 0 0 0-.385-.088.862.862 0 0 0-.385.088c-.118.06-.236.134-.325.237a.877.877 0 0 0-.192.341.74.74 0 0 0-.03.4l-.044 3.272A14.39 14.39 0 0 0 1.6 11.242 14.502 14.502 0 0 0 .285 20.72a14.468 14.468 0 0 0 4.896 8.219 14.375 14.375 0 0 0 8.947 3.346l10.575.045a.594.594 0 0 0 .592-.593v-5.345a.625.625 0 0 0-.237-.49Zm-10.723 3.806a11.899 11.899 0 0 1-7.187-2.473 11.917 11.917 0 0 1-4.2-6.353 11.948 11.948 0 0 1 .517-7.596A11.897 11.897 0 0 1 8.493 7.57l-.014 3.894c-.015.133 0 .267.03.4.044.133.103.252.192.355a.882.882 0 0 0 .325.237.863.863 0 0 0 .77 0c.117-.059.236-.133.324-.237a.877.877 0 0 0 .193-.34.74.74 0 0 0 .03-.4l.014-4.783A11.865 11.865 0 0 1 14.424 6c2.559 0 5.044.844 7.07 2.384a.303.303 0 0 1 .03.46l-5.532 5.078a4.218 4.218 0 0 0-1.597-.325c-.607 0-1.213.118-1.775.37a4.186 4.186 0 0 0-1.45 1.08c-.399.46-.695 1.008-.872 1.585a4.153 4.153 0 0 0-.133 1.807 4.38 4.38 0 0 0 .62 1.703c.326.518.77.947 1.273 1.288a4.394 4.394 0 0 0 1.7.637 4.122 4.122 0 0 0 1.805-.134l7.07 5.376a.31.31 0 0 1 .118.237l-.015 1.895a.297.297 0 0 1-.296.296l-8.105-.03Zm1.73-11.847c0 .326-.103.652-.28.933a1.717 1.717 0 0 1-.754.622c-.311.133-.651.163-.962.089a1.8 1.8 0 0 1-.858-.46 1.643 1.643 0 0 1-.355-1.821c.134-.31.34-.562.622-.755a1.76 1.76 0 0 1 .931-.281c.444 0 .873.178 1.184.503.295.297.473.711.473 1.17Z"
-                      clipRule="evenodd"
-                    />
-                  </g>
-                  <defs>
-                    <clipPath id="a">
-                      <rect
-                        width={26}
-                        height={32}
-                        y={0.329}
-                        fill="#fff"
-                        rx={3}
-                      />
-                    </clipPath>
-                  </defs>
-                </svg>
-                Gitcoin Passport
+               
+<label className="relative inline-flex items-center cursor-pointer">
+  <input type="checkbox" value="" className="sr-only peer" />
+  <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+  <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Toggle me</span>
+</label>
+
               </td>
               <td className="whitespace-nowrap px-3 py-4 space-y-1">
                 <span className="inline-flex items-center gap-x-1.5 rounded-full bg-green-100 px-2 py-0.5 text-sm font-medium text-green-700">
